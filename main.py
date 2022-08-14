4f180e24d82120c284e731d19b37d181fc942c9e
Yash Bellary <bellaryyash23@gmail.com>
2022-08-14 19:17:29 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    text = question["question"]
+    answer = question["correct_answer"]
+    question_bank.append(Question(text, answer))
+
+random.shuffle(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print(f"Congratulation!! You've completed the quiz .")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}.")
